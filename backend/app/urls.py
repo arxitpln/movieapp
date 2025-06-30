674654b0b2c6bb4b70107fe6941989576bd79414
pln <pln@arxit.com>
2025-06-30 16:00:04 +0200

    add API urls

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from app.movies.views import MovieViewSet
+from app.reviews.views import ReviewViewSet
+from app.actors.views import ActorViewSet
+
+router = DefaultRouter()
+router.register(r'movies', MovieViewSet)
+router.register(r'reviews', ReviewViewSet)
+router.register(r'actors', ActorViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
