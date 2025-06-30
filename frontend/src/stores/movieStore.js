0339efa3ee636e2079a5d9a7faf61766cdaa376d
pln <pln@arxit.com>
2025-06-30 20:23:56 +0200

    Upgrade grade without replacing modified values

--- frontend/src/stores/movieStore.js
+++ frontend/src/stores/movieStore.js
@@ -14,37 +14,36 @@ export const useMovieStore = defineStore('movie', {
   actions: {
     async fetchMovies(page = 1) {
       if (page === this.page && this.movies.length > 0) return
-      const response = await axios.get(`${API_BASE}/movies/?page=${page}&count=${MOVIES_PER_PAGE}`)
-      this.movies = response.data.results
-      this.count = response.data.count
-      this.page = page
+      const response = await axios.get(`${API_BASE}/movies/?page=${page}&count=${MOVIES_PER_PAGE}`);
+      this.movies = response.data.results;
+      this.count = response.data.count;
+      this.page = page;
     },
     async fetchMovie(id) {
       if (this.selectedMovie?.id === id) return
-      const response = await axios.get(`${API_BASE}/movies/${id}/`)
-      this.selectedMovie = response.data
+      const response = await axios.get(`${API_BASE}/movies/${id}/`);
+      this.selectedMovie = response.data;
     },
     async updateMovie(id, data) {
       if (this.selectedMovie?.id === id) {
-        const hasChanged = Object.keys(data).some(key => data[key] !== this.selectedMovie[key])
+        const hasChanged = Object.keys(data).some(key => data[key] !== this.selectedMovie[key]);
         if (!hasChanged) return
       }
 
-      const response = await axios.put(`${API_BASE}/movies/${id}/`, data)
-      this.selectedMovie = response.data
-      const index = this.movies.findIndex(m => m.id === id)
+      const response = await axios.put(`${API_BASE}/movies/${id}/`, data);
+      this.selectedMovie = response.data;
+      const index = this.movies.findIndex(m => m.id === id);
       if (index !== -1) {
-        this.movies[index] = response.data
+        this.movies[index] = response.data;
       }
     },
     async addReview(movieId, review) {
       await axios.post(`${API_BASE}/reviews/`, {
         grade: review.grade,
         movie: movieId
-      })
-      if (this.selectedMovie?.id === movieId) {
-        await this.fetchMovie(movieId)
-      }
+      });
+      const response = await axios.get(`${API_BASE}/movies/${movieId}/`);
+      this.selectedMovie.average_grade = response.data.average_grade;
     }
   }
 })
