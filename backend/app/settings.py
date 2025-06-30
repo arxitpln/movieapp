86660c97a3f7b61951d65f6dc81caeac8765774b
pln <pln@arxit.com>
2025-06-30 20:21:17 +0200

    Manage cors

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,9 +55,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# For Debug only
-# MIDDLEWARE.insert(0, 'corsheaders.middleware.CorsMiddleware')
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS  = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
 
 ROOT_URLCONF = 'app.urls'
 
