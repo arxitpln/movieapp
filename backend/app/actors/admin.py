30d65009f8c56bda4be4c65883ba9f8399b769de
pln <pln@arxit.com>
2025-06-30 15:52:46 +0200

    Init actors logic

--- backend/app/actors/admin.py
+++ backend/app/actors/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Actor
 
-# Register your models here.
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name")
+    search_fields = ("first_name", "last_name")
--- backend/app/actors/views.py
+++ backend/app/actors/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Actor
+from .serializers import ActorSerializer
 
-# Create your views here.
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
